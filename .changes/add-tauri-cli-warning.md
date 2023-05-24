41d2f3ed989c0e1123896963552fea5b92b7d5a2
Lucas Nogueira <lucas@tauri.studio>
2023-05-23 22:45:04 -0300

    feat(tauri.js): add warning

---
+++ .changes/add-tauri-cli-warning.md
@@ -0,0 +1,6 @@
+---
+"tauri.js": minor
+---
+
+The package is no longer maintained since it was only used for the Tauri alpha.
+
---
+++ cli/tauri.js/warning.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10; i++) {
+  console.error(
+    `This package represents the Tauri alpha CLI. To use the stable Tauri CLI, see \`@tauri-apps/cli\`: https://www.npmjs.com/package/@tauri-apps/cli`
+  )
+}
